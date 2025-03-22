e78cb5461fefeceb472e0d10634f726946f74da3
Toan <bachmatoan2910@gmail.com>
2025-03-22 14:31:53 -0400

    draft supplier page - tho it's still empty

--- src/components/sections/hero.tsx
+++ src/components/sections/hero.tsx
@@ -35,7 +35,7 @@ export const HeroSection = () => {
           </p>
 
           <div className="space-y-4 md:space-y-0 md:space-x-4">
-            <Link href="/dashboard">
+            <Link href="/company-dashboard">
               <Button className="w-5/6 md:w-1/4 font-bold group/arrow ">
                 Get Started
                 <ArrowRight className="ml-2 group-hover/arrow:translate-x-1 transition-transform" />
